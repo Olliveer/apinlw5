4193d528f4a2544887d9980e915944927cae49bc
José Oliveira <j.oliveira90@live.com>
2021-04-25 20:48:36 -0300

    add MessagesService

---
+++ src/services/MessagesService.ts
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
+import { getCustomRepository } from 'typeorm';
+import MessagesRepository from '../repositories/MessagesRepository';
+
+interface IMessageCreate {
+  admin_id: string;
+  text: string;
+  user_id: string;
+}
+
+class MessagesService {
+  async create({ admin_id, text, user_id }: IMessageCreate) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    const message = messagesRepository.create({
+      admin_id,
+      text,
+      user_id,
+    });
+
+    await messagesRepository.save(message);
+
+    return message;
+  }
+}
+
+export default MessagesService;
