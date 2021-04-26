7b53783aa5dcc1d9bbb6188f4fb7cdae3f6d8376
José Oliveira <j.oliveira90@live.com>
2021-04-25 21:02:45 -0300

    messages controller and messages service

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import MessageController from './controllers/MessageController';
 import SettingController from './controllers/SettingsController';
 import UsersController from './controllers/UsersController';
 
 const routes = Router();
 const settingController = new SettingController();
 const usersController = new UsersController();
+const messagesController = new MessageController();
 
 routes.post('/settings', settingController.create);
 routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export default routes;
